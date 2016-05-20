afb845b6d874cc55892853043dc47ec1415fa4a3
Karthik <karthik.kasala@gmail.com>
2016-05-20 11:31:42 -0400

    added exercise 2

---
+++ ex2.py
@@ -0,0 +1,9 @@
+# comments, to read what code does in program
+# Anything after the # is ignored by python when running program
+
+print "I could have code like this." # and the comment after is ignored
+
+#You can also use comments to disable code from running
+# print "This won't run."
+
+print "This will run."
