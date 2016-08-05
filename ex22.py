1ceb634b469e648bcd1ba30d655b50ed7c349703
Karthik <karthik.kasala@gmail.com>
2016-08-04 21:52:14 -0400

    completed exercises 20 through 24

---
+++ ex22.py
@@ -0,0 +1,29 @@
+### list of characters used in Exercises 1 through 21.
+
+## Exercise 1:
+
+# octothorpe - creates comments/code that will not be used
+
+# print: displays strings, variables, functions,
+print
+
+# strings
+" dfadfa " 'dsfat' '''fdasfasdf'''
+
+# mathematical characters
+ + - * /
+
+ # division remainder
+ //
+
+ # exponents
+ **
+
+# modulus, which helps represent things for print functions
+%
+
+# for formatting
+%s
+
+# for debugging information about something
+%r
