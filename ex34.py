d7ee47edfd8bf8838305c9591b0308759b3e4e87
Karthik <karthik.kasala@gmail.com>
2016-08-15 12:41:07 -0400

    added .gitignore and completed ex34 and ex35 study drills

---
+++ ex34.py
@@ -0,0 +1,25 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# 1. The second(2nd) animal is at 1 and is a python.
+print animals[1]
+
+# 2. The third(3rd) animal is at 2 and is a peacock.
+print animals[2]
+
+# 3. The first(1st) animal is at 0 and is a bear.
+print animals[0]
+
+# 4. The fourth(4th) animal is at 3 and is a kangaroo.
+print animals[3]
+
+# 5. The fifth(5th) animal is at 4 and is a whale.
+print animals[4]
+
+# 6. The third(3rd) animal is at 2 and is a peacock.
+print animals[2]
+
+# 7. The sixth(6th) animal is at 5 and is a platypus.
+print animals[5]
+
+# 8. The fifth(5th) animal is at 4 and is a whale.
+print animals[4]
